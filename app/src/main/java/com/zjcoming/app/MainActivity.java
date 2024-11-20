f1c832122aaab004d12051010a4de095578112ea
zhangjun <specialzhang@didiglobal.com>
2024-11-20 16:21:26 +0800

    first commit

---
+++ app/src/main/java/com/zjcoming/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zjcoming.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
